63c9088375ddc61545c4f02626b1fdc878a2457e
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-10 16:53:50 -0300

    Grid table

--- cursoreactionic/src/pages/customer/CustomerList.tsx
+++ cursoreactionic/src/pages/customer/CustomerList.tsx
@@ -1,6 +1,7 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../../components/ExploreContainer';
+import { add } from 'ionicons/icons';
 
 const CustomerList: React.FC = () => {
 
@@ -24,7 +25,34 @@ const CustomerList: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />assadadsadssas
+        <IonCard>
+        <IonTitle>Gestión de Clientes</IonTitle>
+        <IonItem>
+          <IonButton color="primary" fill='solid' slot="end" size="default">
+          <IonIcon icon={add}/>
+            Agregar Cliente
+          </IonButton>
+        </IonItem>
+        <IonGrid className="table">
+        <IonRow >
+        <IonCol>Nombre</IonCol>
+          <IonCol>Email</IonCol>
+          <IonCol>Teléfono</IonCol>
+          <IonCol>Dirección</IonCol>
+          <IonCol>Acciones</IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonGrid className="table">
+        <IonRow>
+        <IonCol>Juan Perez</IonCol>
+          <IonCol>juanperez@mail.com</IonCol>
+          <IonCol>34122332412</IonCol>
+          <IonCol>Avenida siempre viva 123</IonCol>
+          <IonCol>Boton Editar</IonCol>
+        </IonRow>
+      </IonGrid>
+      </IonCard>
       </IonContent>
     </IonPage>
   );
