98d4e62ecd1c28baaa4d8059cbc3b9dc1fad7817
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-11 16:32:10 -0300

    Costumer controller

--- springjava/javaSpring/src/main/java/com/example/demo/DemoApplication.java
+++ springjava/javaSpring/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.entities.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- springjava/javaSpring/src/main/java/com/example/demo/entities/Customer.java
+++ springjava/javaSpring/src/main/java/com/example/demo/entities/Customer.java
@@ -1,4 +1,24 @@
 package com.example.demo.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//Esto me generara getters y setters para todas las variables si quisiera elegirlo pondria arriba de la variable directamente
+@ToString // Me genera el metodo toString en todas las variables
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Customer {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String address;
+
+    private String phone;
 }
