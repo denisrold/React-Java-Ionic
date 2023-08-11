118eb35ae605775ced2830e6c114c075f3b1c853
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-11 00:27:54 -0300

    interface

---
+++ cursoreactionic/src/pages/customer/Costumer.ts
@@ -0,0 +1,9 @@
+//Usamos interfaces para definir la estructura de Datos.
+export default interface customer {
+id:string;
+firstname:string;
+lastname:string;
+email:string;
+phone:string;
+address:string;
+};
