a67310e2ce3dc378f1379861fe41c56d02e95783
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-12 01:41:03 -0300

    new dependencies

--- springjava/javaSpring/src/main/java/com/example/demo/controllers/CustomersController.java
+++ springjava/javaSpring/src/main/java/com/example/demo/controllers/CustomersController.java
@@ -1,8 +1,19 @@
 package com.example.demo.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.example.demo.entities.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
 public class CustomersController {
-
+    @GetMapping("/api/customers")
+    public List<Customer> getAll(){
+     List<Customer> customers = new ArrayList<>();
+     customers.add(new Customer());
+     return customers;
+ }
 }
