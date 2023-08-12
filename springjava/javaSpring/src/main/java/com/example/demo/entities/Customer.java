bac18a320fd1b78724e9b9bd030301a34e4cd9dc
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-12 01:52:07 -0300

    Repositories

--- springjava/javaSpring/src/main/java/com/example/demo/entities/Customer.java
+++ springjava/javaSpring/src/main/java/com/example/demo/entities/Customer.java
@@ -1,16 +1,21 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 //Esto me generara getters y setters para todas las variables si quisiera elegirlo pondria arriba de la variable directamente
+@Entity //le digo que customers es una entidad
+@Table(name = "customers")//indico el nombre de la tabla
 @ToString // Me genera el metodo toString en todas las variables
 @EqualsAndHashCode
-@Getter
-@Setter
+@Getter @Setter
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String firstname;
 
